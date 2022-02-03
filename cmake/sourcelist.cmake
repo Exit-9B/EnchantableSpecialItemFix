3804b15f2f5613477e922643db0e9ff2eede2a5a
Parapets <southwest85b@gmail.com>
2022-02-03 09:35:08 +0000

    Remove unused sourcelist

--- cmake/sourcelist.cmake
+++
@@ -1,9 +0,0 @@
-set(SOURCE_FILES ${SOURCE_FILES}
-    src/PCH/PCH.cpp
-    src/PCH/PCH.h
-    src/RE/Offset.h
-    src/REL/Pattern.h
-    src/main/ItemCardManager.cpp
-    src/main/ItemCardManager.h
-    src/main/SKSEPlugin.cpp
-)
