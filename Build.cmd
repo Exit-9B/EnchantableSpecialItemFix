ad65ce7ada4889f6ec9ab4da8f7aa49d59801a05
Parapets <southwest85b@gmail.com>
2022-03-18 12:39:10 +0000

    Set up FOMOD and distribution

---
+++ Build.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+md "package\SkyrimSE"
+md "package\SkyrimVR"
+
+cmake --preset vs2019-windows --check-stamp-file "build/CMakeFiles/generate.stamp" || goto :error
+cmake --build build --config Release || goto :error
+cmake --install build --component "Fomod" --prefix "package" || goto :error
+cmake --install build --component "SKSEPlugin" --prefix "package/SkyrimSE" || goto :error
+
+cmake --preset vs2019-windows-vr --check-stamp-file "buildVR/CMakeFiles/generate.stamp" || goto :error
+cmake --build buildVR --target "EnchantableSpecialItemFix" --config Release || goto :error
+cmake --install buildVR --component "SKSEPlugin" --prefix "package/SkyrimVR" || goto :error
+
+pushd package
+7z a -r -t7Z "..\EnchantableSpecialItemFix.7z" *
+popd
+goto :EOF
+
+:error
+exit /b %errorlevel%
